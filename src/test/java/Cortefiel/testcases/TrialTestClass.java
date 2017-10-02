c46df641e4c85dfd480c8080b06925265ea15cea
syam.s@outlook.com <Liverp00l>
2017-10-02 12:28:41 +0200

    started on TrialTestClass,using the public static void main

--- src/test/java/Cortefiel/testcases/TrialTestClass.java
+++ src/test/java/Cortefiel/testcases/TrialTestClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TrialTestClass {
     public static void main (String[] args){
         WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         driver = new ChromeDriver();
         String sURL = "https://"+"storefront"+":"+"storefront"+"@"+"development.cortefiel.com/es/es";
         driver.get(sURL);
