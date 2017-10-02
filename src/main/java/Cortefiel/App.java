590bf8f58ee1a036107e1265a6e53393cf8faacf
syam.s@outlook.com <Liverp00l>
2017-10-02 12:15:51 +0200

    First commit

---
+++ src/main/java/Cortefiel/App.java
@@ -0,0 +1,13 @@
+package Cortefiel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Cortefiel/TrialTestClass.java
@@ -0,0 +1,10 @@
+package Cortefiel;
+
+import org.openqa.selenium.WebDriver;
+
+public class TrialTestClass {
+    public static void main (String[] args){
+        WebDriver driver;
+
+    }
+}
